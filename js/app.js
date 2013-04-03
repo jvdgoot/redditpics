b105c5cf614c3f2e02cdcc6861472eb11b4f00d6
Jeffry van der Goot <jvdgoot@gmail.com>
2013-04-04 01:08:26 +0200

    lesser limit, but pagination added, so can get more than 100 pics

--- js/app.js
+++ js/app.js
@@ -1,46 +1,51 @@
-$(document).ready(function(){
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+      vars[key] = value;
+  });
+  return vars;
+}
 
-  function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
-  }
+function getRedditPics(reddits, limit, after) {
+  $.getJSON("http://www.reddit.com/r/"+reddits+".json?limit="+limit+"&after="+after+"&jsonp=?", function(json){
+    addPicsToDOM(json);
+  });
+}
 
-    var urlParameters = getUrlVars(["reddits"]);
-
-
-
-
-    $.getJSON("http://www.reddit.com/r/"+urlParameters.reddits+".json?limit=100&jsonp=?", function(json){
-
-
-      $.each(json.data.children,function(i,reddit){
+function addPicsToDOM(redditJSON) {
+  $("body").attr("data-after", redditJSON.data.after);
+  var pagenr = parseFloat($("body").attr("data-iteration")) + 1;
+  $.each(redditJSON.data.children,function(i,reddit){
 
        if (reddit.data.is_self === false && reddit.data.thumbnail != "" ) {
 
-        // console.log(reddit.data.url.search("jpg"));
-
           if (reddit.data.url.search("jpg") != -1 || reddit.data.url.search("png") != -1 || reddit.data.url.search("jpeg") != -1)
           {
-            $("section#pics").append('<a href="'+reddit.data.url+'" class="picture" target="_blank"><img src="'+reddit.data.url+'"></a>');
+            $("section#pics").append('<a href="'+reddit.data.url+'" class="picture nr'+pagenr+'" target="_blank"><img src="'+reddit.data.url+'"></a>');
           }
         }
       });
+      $("#pics").imagesLoaded(function(){
+        $("#pics").masonry('reload');
+        $(".amount_of_pics").html($("#pics").find(".picture").length);
+      });
+}
+
+function morePics(){
+  getRedditPics($("body").attr("data-reddits"), 20, $("body").attr("data-after"));
+}
+
+$(document).ready(function(){
+  var reddits = getUrlVars().reddits;
+
+  $("body").attr({
+    "data-reddits": reddits,
+    "data-iteration": 0}
+    );
+
+  $("#pics").masonry({itemSelector: '.picture'});
+
+  getRedditPics($("body").attr("data-reddits"), 20, "");
 
 
-    }).success(function(){
-        var container = $("#pics");
-        var images = $(container).find(".picture");
-        var images_count = images.length;
-        $(".amount_of_pics").html(images_count);
-
-        $("#pics").imagesLoaded(function(){
-          $("#pics").masonry({
-            itemSelector: '.picture'
-          });
-        });
-    });
-
-  });
+});
